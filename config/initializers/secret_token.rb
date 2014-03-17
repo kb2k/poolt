d75071602a72234bf6409e9ac6e8041fcdd72b4c
Ant0ha <wapwork@bk.ru>
2014-03-17 23:31:58 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poolt::Application.config.secret_key_base = '4860febfed0300d834dea2217cd7259704570380811feb399c4fb4a286751524fea3db81c97d800379dee80850c695d79fc063352b1dccc4429fdbb8b5a59f31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poolt::Application.config.session_store :cookie_store, key: '_poolt_session'
